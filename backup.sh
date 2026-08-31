c4d52e0a2450cd75b7227934e6efed7629458941
Vivien Henry <vivien.henry@inductivebrain.fr>
2026-08-31 13:59:40 +0200

    wip backup

--- backup.sh
+++ backup.sh
@@ -1,25 +1,35 @@
 #!/bin/bash
 # RUN THIS SCRIPT DIRECTLY FROM THE HOST MACHINE, NOT FROM WITHIN THE DOCKER CONTAINER!
-BACKUP_DIR="`dirname $0`/backups"
+
+if [ -z "$BACKUP_DIR" ]; then
+    echo "\$BACKUP_DIR must be set !"
+    exit 1
+fi
+
+if [ -z "$SVN_DOCKER_VOLUME_DIR" ]; then
+    echo "\$SVN_DOCKER_VOLUME_DIR must be set !"
+    exit 1
+fi
+    
+
 WD=`pwd`
-cd $BACKUP_DIR/..
 
 echo '---'
 echo 'Shutting down SVN Server...'
 
 # Temporarily shut down the SVN Server until the backup procedure has finished.
-sudo docker-compose down
+# docker-compose down svn
 
 echo "Backing up the entire SVN Server instance into $BACKUP_DIR/"
 echo 'This might take a while, perhaps go grab a coffee...'
 
 # Ensure that the backup output directory exists.
-sudo mkdir -p $BACKUP_DIR
+mkdir -p $BACKUP_DIR
+cd $BACKUP_DIR/..
 
 # Make sure that this is the full, 
 # absolute path to the directory where
 # the SVN Server container volume is mounted to.
-VOL_DIR=${SVN_DOCKER_VOLUME_DIR:-/mnt/dockervolumes/svn/}
 
 # Get the current time for the output file name.
 TIMESTAMP=`date +"%Y-%m-%d-%H-%M-%S"`
@@ -28,7 +38,7 @@ TIMESTAMP=`date +"%Y-%m-%d-%H-%M-%S"`
 OUTPUT_FILE_PATH=$BACKUP_DIR/$TIMESTAMP-svn-server-backup.tar.gz
 
 # Compress the entire SVN Server instance into a tar.gz archive.
-sudo tar -zcvf $OUTPUT_FILE_PATH $VOL_DIR
+tar -zcvf $OUTPUT_FILE_PATH --directory $SVN_DOCKER_VOLUME_DIR .
 
 echo '---'
 echo "SVN Server backup was exported into $OUTPUT_FILE_PATH"
@@ -36,5 +46,5 @@ echo 'Maybe consider encrypting the archive using a strong password + 7z (choose
 echo '---'
 echo 'Restarting SVN Server after backup...'
 
-sudo docker-compose up -d
+# docker-compose up -d svn
 cd $WD
