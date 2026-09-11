23e0927cf37ef9e14f8a5f99813bc7ebe7a94aa1
Vivien Henry <vivien.henry@inductivebrain.fr>
2026-09-11 13:54:49 +0200

    update backup, include inventree

--- backup.sh
+++ backup.sh
@@ -14,25 +14,41 @@ echo 'Shutting down Proxy Server...'
 # Temporarily shut down the SVN Server until the backup procedure has finished.
 docker-compose down plume-proxy
 
-echo "Backing up the entire SVN Server instance into $BACKUP_DIR"
+#################################################
+### SVN
+#################################################
+
+# Get the current time for the output file name.
+TIMESTAMP=`date +"%Y%m%d__%H-%M-%S"`
+
+echo "Backing up the entire SVN Server instance into ${$BACKUP_DIR}/${$TIMESTAMP}"
 echo 'This might take a while, perhaps go grab a coffee...'
 
 # Ensure that the backup output directory exists.
-mkdir -p "$BACKUP_DIR"
-
-# Get the current time for the output file name.
-TIMESTAMP=`date +"%Y-%m-%d-%H-%M-%S"`
+mkdir -p "${$BACKUP_DIR}/${$TIMESTAMP}"
 
 # Construct the output file path using the timestamp.
-OUTPUT_FILE=$TIMESTAMP-svn-server-backup.tar.gz
+OUTPUT_FILE=svn-server-backup.tar.gz
 
 # Compress the entire SVN Server instance into a tar.gz archive.
-docker run --rm --volumes-from svn-server -v "${BACKUP_DIR}":/backups ubuntu tar -zcvf /backups/$OUTPUT_FILE --directory /home/svn .
+docker run --rm --volumes-from svn-server -v "${$BACKUP_DIR}/${$TIMESTAMP}":/backups ubuntu tar -zcvf /backups/$OUTPUT_FILE --directory /home/svn .
 
 echo '---'
-echo "SVN Server backup was exported into ${BACKUP_DIR}/${OUTPUT_FILE_PATH}"
+echo "SVN Server backup was exported into ${$BACKUP_DIR}/${$TIMESTAMP}}/${OUTPUT_FILE_PATH}"
 echo 'Maybe consider encrypting the archive using a strong password + 7z (choose ENCRYPT FILE NAMES TOO  when asked).'
 echo '---'
+
+
+#################################################
+### Inventree
+#################################################
+docker compose run --rm -v "${$BACKUP_DIR}/${$TIMESTAMP}":/home/inventree/data/backup inventree-server invoke backup
+
+
+# Restart proxy
 echo 'Restarting Proxy Server after backup...'
 
 docker-compose up -d plume-proxy
+
+
+echo 'DONE ! Check and save the files in a safe place :)'
